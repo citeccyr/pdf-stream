1d50cc29852151901c9cb979534b06523cb5252d
Mrau Hu <mrauhu@yandex.ru>
2017-03-06 06:54:43 +0300

    Created `lib/text` function for getting text stream from PDF.

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 module.exports.PDFReadable = require('./lib/PDFReadable');
 module.exports.PDFStringifyTransform = require('./lib/PDFStringifyTransform');
+module.exports.text = require('./lib/text');
