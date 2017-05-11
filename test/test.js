535eb545ac78edefbc2163a70ad302a63055e72b
Mrau Hu <mrauhu@yandex.ru>
2017-05-12 02:45:18 +0300

    Update `test.js`: added `global.DOMParser` and test file.
    Change whitespace in test to ``.

--- test/test.js
+++ test/test.js
@@ -5,9 +5,12 @@ const PDFStringifyTransform = require('../lib/PDFStringifyTransform');
 
 // Download remote file
 global.XMLHttpRequest = require('xhr2'); // for PDFJS
+global.DOMParser = require('xmldom').DOMParser;
+
+const src = 'https://socionet.ru/~vic/j-neicon/aerospace/y:2016:i:5:p:16-30.pdf';
 //const src = 'http://dspacecris.eurocris.org/bitstream/11366/526/1/CRIS2016_paper_40_Parinov.pdf';
 //const src = 'http://nevolin.socionet.ru/files/2014_Nevolin_rfbr.pdf';
-const src = 'http://www.pdt-journal.com/jour/article/viewFile/112/145.pdf';
+//const src = 'http://www.pdt-journal.com/jour/article/viewFile/112/145.pdf';
 
 // Use local file
 //const fs = require('fs');
@@ -20,7 +23,7 @@ new PDFReadable(src)
   .on('error', function(err){
     console.error('PDFReadable error', err);
   })
-  .pipe(new PDFStringifyTransform({whitespace: ' '}))
+  .pipe(new PDFStringifyTransform({whitespace: ''}))
   .pipe(process.stdout)
 ;
 
